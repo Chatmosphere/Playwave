9ec0de5458de63e33faa45d50784729ab52857ea
David Grieshammer <dkgrieshammer@gmail.com>
2021-12-04 12:06:47 +0000

    Cleanup

--- plays/sample.js
+++ plays/sample.js
@@ -29,14 +29,15 @@ function getRandomInt(min, max) {
       // '--use-file-for-fake-audio-capture=assets/fakeAudioStream.wav',
     ]
   }
-  // '--use-file-for-fake-video-capture=assets/example.y4m',
   
+  // generate Random User Name
   var randomName = faker.name.findName();
+  
+  // Launch Browser & wait for new Page to open
   const browser = await chromium.launch(launchOptions);
   const page = await browser.newPage()
-  // Create pages, interact with UI elements, assert values
-
-  console.log(process.env.TARGET_URL);
+  
+  // Go to Target URL
   await page.goto(process.env.TARGET_URL);
   
   // Click text=Friendly Sphere
@@ -46,7 +47,6 @@ function getRandomInt(min, max) {
   await page.fill('[placeholder="Friendly Sphere"]', randomName);
   
   // Press Enter
-  // await page.press('#DragElement > .sc-kfzAmx.gpdyeJ', 'Enter');
   await page.press(`.sc-kfzAmx.gpdyeJ`, 'Enter');
 
   // Drag #DragElement video
@@ -55,20 +55,21 @@ function getRandomInt(min, max) {
   let mouse = page.mouse
   mouse.move(elBoundingBox.x +elBoundingBox.width/2, elBoundingBox.y +elBoundingBox.height/2)
   await page.waitForTimeout(100);
-  // mouse.click(elBoundingBox.x +elBoundingBox.width/2, elBoundingBox.y +elBoundingBox.height/2)
   mouse.down()
   await page.waitForTimeout(500);
   mouse.move(getRandomInt(-600,600), getRandomInt(-600,600));
   await page.waitForTimeout(1000);
   mouse.up()
+
   // Time Out
   await page.waitForTimeout(20000);
 
   // Screenshot with current time - page.screenshot({ path: `example.png` });
   const tmpTm = Date.now()
   await page.screenshot({
-      path: `${process.env.OUTPUT_PATH}screenshot-${tmpTm}.png`,
+      path: `${process.env.OUTPUT_PATH}/screenshot-${tmpTm}.png`,
   });
-  // Close Browser
+
+  // Close Browser if you want - currently we want ongoing users
   // await browser.close();
 })();
